a40d69adf0cf1e889e9b207b1dc0282f3b60e6b5
benkissi <benkissi111@gmail.com>
2019-12-27 21:46:16 +0000

    complete project

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
+
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+const Subscription = require("./resolvers/Subscription");
+const Vote = require("./resolvers/Vote");
+
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Link,
+  Subscription,
+  Vote
+};
+
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+  context: request => {
+    return { ...request, prisma };
+  }
+});
+
+server.start(() => console.log("server started on 4000"));
